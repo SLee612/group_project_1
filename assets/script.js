0931aec6ecfab9637478900dba4499579062cb00
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2021-03-24 11:46:42 -0500

    added news source api, and changed to create card dynamically

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,10 @@ var searchForm= $('.form-control');
 //clear historybutton
 var clearHistoryBtn = $('#clear-history');
 var searchedNewsListEl = $('.list-group');
-var searchedNewsListItem = $('<li>');
-    searchedNewsListItem.attr('class', 'list-group-item');
 var newsSearchInput = ''
 var savedSearches = []
+var newsStory= $('#News-Story');
+
 
 var searchInputSaved = JSON.parse(localStorage.getItem('searches'));
 
@@ -39,11 +39,50 @@ function getFormInfo(){
    if(!newsSearchInput){
        return
    }
+   var searchedNewsListItem = $('<li>');
+    searchedNewsListItem.attr('class', 'list-group-item');
 
     searchedNewsListItem.text(newsSearchInput);
 
     searchedNewsListEl.append(searchedNewsListItem);
     searchForm.val('');
+
+    var articleSearch = `https://gnews.io/api/v4/search?q=${newsSearchInput}&country=us&token=4c6477a39ac0888785968fdb8d31562e`;
+        $.ajax({
+            url:articleSearch,
+            method:'GET',
+        }).then(function(response){
+            console.log(response)
+            newsStory.text("");
+            var newsRow =$('<row>');
+            newsRow.addClass('row')
+            newsStory.append(newsRow)
+            for ( var i = 0; i < 3; i++ ){
+              var newsCard =$('<figure>');
+              newsCard.addClass('col-md-4');
+              
+              var newsTitle = $('<h7>')
+              newsTitle.text(response.articles[i].title);
+              var newsDescription = $('<p>')
+              newsDescription.text(response.articles[i].description);
+            //   var newsImage = $('<img>')
+            //   newsImage.addClass('newspic')
+            //   newsImage.attr('src','response.articles[i].image')
+              var newsSourceName= $('<p>')
+              newsSourceName.text((response.articles[i].source.name))
+              var newsSourceUrl= $('<p>')
+              newsSourceUrl.text((response.articles[i].source.url))  
+
+              newsCard.append(newsTitle)
+              newsCard.append(newsDescription)
+            //   newsCard.append(newsImage)
+              newsCard.append(newsSourceName)
+              newsCard.append(newsSourceUrl)
+
+              newsRow.append(newsCard)
+            }
+
+        });
 
     storeInputToLocalStorage()
   }
